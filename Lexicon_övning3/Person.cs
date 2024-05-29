22d78d87c2cc4503e78e6bce81733145709ce001
Heithem Ghasali <heithem.gha98@gmail.com>
2024-05-29 11:46:56 +0200

    uppgifft 1

---
+++ Lexicon_övning3/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon_övning3
+{
+    internal class Person
+    {
+
+        private int age;
+        private string fName;
+        private string lName;
+        private double height;
+        private double weight;
+
+
+        public int Age 
+        {
+            get { return age; } 
+            set { age = value; }
+        
+        }
+
+
+        public string FName
+        {
+            get { return fName; }
+            set { fName = value; }
+
+        }
+
+        public string LName
+        {
+            get { return lName; }
+            set { fName = value; }
+
+        }
+    }
+}
