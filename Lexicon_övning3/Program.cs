3d830988764e4fc797b6b95f2daf5f0d4e6df549
Heithem Ghasali <heithem.gha98@gmail.com>
2024-05-29 10:40:21 +0200

    Add project files.

---
+++ Lexicon_övning3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lexicon_övning3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
