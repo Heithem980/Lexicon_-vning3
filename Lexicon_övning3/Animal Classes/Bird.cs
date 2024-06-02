9a44e8883f3eb412977715f1b68c700710358668
Heithem Ghasali <heithem.gha98@gmail.com>
2024-06-02 20:36:59 +0200

    3.3 (7.)

--- Lexicon_övning3/Animal Classes/Bird.cs
+++ Lexicon_övning3/Animal Classes/Bird.cs
@@ -12,6 +12,7 @@
 
         public override void DoSound()
         {
+            Console.WriteLine("Chirp");
         }
     }
 }
--- Lexicon_övning3/Animal Classes/Worm.cs
+++ Lexicon_övning3/Animal Classes/Worm.cs
@@ -12,7 +12,7 @@
 
         public override void DoSound()
         {
-
+            Console.WriteLine("Silent");
         }
     }
 }
